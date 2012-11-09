04acbc638908ed5d9e5dad0869f2f707ce0d100e
IntPtr <admin@tsnake.net>
2012-11-09 20:26:37 +0800

    Create Project

---
+++ TBlock-1.1.0.js
@@ -0,0 +1,230 @@
+(function(window){
+    var TBlock={};
+    var prototype={
+        show:function(){
+            this.scene.add(this.block);
+            this.visible=true;
+        },
+        hide:function(){
+            this.scene.remove(this.block);
+            this.visible=false;
+        },
+        isVisible:function(){
+            return this.visible;
+        },
+        position:function(){
+            return [this.block.position.x,-this.block.position.z];
+        },
+        rect:function(){
+            var position=this.position();
+            return [position[0]-5,position[1]-5,10,10]
+        },
+        move:function(x,y){
+            this.block.position.x=x;
+            this.block.position.z=-y;
+        },
+        moveBy:function(x,y){
+            this.block.position.x+=x;
+            this.block.position.z-=y;
+        },
+        contains:function(x,y){
+            var rect=this.rect();
+            if(rect[0]<=x && x<=rect[0]+rect[2] && rect[1]<=y && y<=rect[1]+rect[3]){
+                return true;
+            }else{
+                return false;
+            }
+        },
+        containsRect:function(x,y,width,height){
+            return this.contains(x,y) && this.contains(x+width,y) && this.contains(x,y+height) && this.contains(x+width,y+height);
+        },
+        collision:function(x,y,width,height){
+            return this.contains(x,y) || this.contains(x+width,y) || this.contains(x,y+height) || this.contains(x+width,y+height);
+        }
+    };
+    TBlock.Block=function(scene){
+        this.scene=scene;
+        this.visible=false;
+        this.block=new THREE.Mesh(
+            new THREE.CubeGeometry(
+                10,
+                10,
+                10
+            )
+        );
+    };
+    TBlock.Block.prototype=prototype;
+    TBlock.User=function(scene){
+        this.scene=scene;
+        this.visible=false;
+        this.block=new THREE.Mesh(
+            new THREE.SphereGeometry(
+                5
+            )
+        );
+    };
+    TBlock.User.prototype=prototype;
+    TBlock.Game=function(parent){
+        parent=parent || document.body;
+        var scene_width=400; //画布宽度
+        var scene_height=400; //画布高度
+        var width=200; //场景宽度
+        var height=400; //场景高度
+        var fps=60; //帧频
+        var max=4; //每行最高方块数
+        var count=100; //方块池大小
+        var speed=3; //前进速度
+        var move_speed=5; //左右移动速度
+        var slow_move_speed=2; //慢速左右移动速度
+        var frames=60; //每隔多少帧为一行
+        var scene=new THREE.Scene();
+        var camera=new THREE.PerspectiveCamera(45,scene_width/scene_height,0.1,1000);
+        var renderer=new THREE.CanvasRenderer();
+        scene.add(camera);
+        camera.position.x=width/2;
+        camera.position.y=150;
+        camera.position.z=250;
+        camera.rotation.x=-0.25;
+        renderer.setSize(scene_width,scene_height);
+        parent.appendChild(renderer.domElement);
+        var blocks=[];
+        for(var i=0;i<count;i++){
+            blocks[i]=new TBlock.Block(scene);
+        }
+        var user=new TBlock.User(scene);
+        user.move(width/2,10);
+        user.show();
+        var time;
+        var isdown;
+        var temp;
+        var init=function(){
+            for(var i=0;i<count;i++){
+                blocks[i].hide();
+            }
+            time=new Date().getTime();
+            isdown={
+                left:false,
+                right:false,
+                up:false,
+                down:false,
+                slow:false
+            };
+            temp=0;
+        };
+        var timer=function(){
+            var now=new Date().getTime();
+            if(now-time>=1000/fps){
+                var times=Math.floor((now-time)/(1000/fps));
+                game(times);
+                time+=times*(1000/fps);
+            }
+            setTimeout(timer,0);
+        };
+        var game=function(times){
+            for(var i=0;i<times;i++){
+                var hidden=[];
+                for(var j=0;j<count;j++){
+                    if(blocks[j].isVisible()){
+                        blocks[j].moveBy(0,-speed);
+                        if(blocks[j].rect()[1]+blocks[j].rect()[3]<0){
+                            blocks[j].hide();
+                        }
+                    }else{
+                        hidden.push(blocks[j]);
+                    }
+                }
+                temp++;
+                if(temp==frames){
+                    temp=0;
+                    for(var j=0;j<max && j<hidden.length;j++){
+                        hidden[j].show();
+                        while(true){
+                            hidden[j].move(Math.floor(Math.random()*(width-10)+5),height);
+                            var flag=0;
+                            for(var k=0;k<j;k++){
+                                var rect=hidden[j].rect();
+                                if(hidden[k].collision(rect[0],rect[1],rect[2],rect[3])){
+                                    flag=1;
+                                    break;
+                                }
+                            }
+                            if(!flag){
+                                break;
+                            }
+                        }
+                    }
+                }
+                var s=isdown.slow?slow_move_speed:move_speed;
+                if(isdown.left){
+                    if(user.position()[0]>=10){
+                        user.moveBy(-s,0);
+                        camera.position.x-=s;
+                    }
+                }
+                if(isdown.right){
+                    if(user.position()[0]<=width-10){
+                        user.moveBy(s,0);
+                        camera.position.x+=s;
+                    }
+                }
+                for(var j=0;j<count;j++){
+                    if(blocks[j].isVisible()){
+                        var rect=blocks[j].rect();
+                        if(user.collision(rect[0],rect[1],rect[2],rect[3])){
+                            alert("Game Over!!!");
+                            init();
+                        }
+                    }
+                }
+            }
+            renderer.render(scene,camera);
+        };
+        window.onkeydown=function(e){
+            e=e||window.event;
+            var key=e.which || e.keyCode;
+            switch(key){
+                case 37:
+                    isdown.left=true;
+                    break;
+                case 38:
+                    isdown.up=true;
+                    break;
+                case 39:
+                    isdown.right=true;
+                    break;
+                case 40:
+                    isdown.down=true;
+                    break;
+                case 16:
+                    isdown.slow=true;
+                    break;
+            }
+
+        };
+        window.onkeyup=function(e){
+            e=e||window.event;
+            var key=e.which || e.keyCode;
+            switch(key){
+                case 37:
+                    isdown.left=false;
+                    break;
+                case 38:
+                    isdown.up=false;
+                    break;
+                case 39:
+                    isdown.right=false;
+                    break;
+                case 40:
+                    isdown.down=false;
+                    break;
+                case 16:
+                    isdown.slow=false;
+                    break;
+            }
+
+        };
+        init();
+        timer();
+    };
+    window.TBlock=TBlock;
+})(window);
